ed0d16644495e769ad56440084d0659a93b8a070
eric <eric123602@gmail.com>
2024-07-21 20:27:56 +0800

    final version

--- lab2/model/SCCNet.py
+++ lab2/model/SCCNet.py
@@ -7,7 +7,6 @@ import logging
 
 # init logger
 logging.basicConfig(level=logging.DEBUG)
-# reference paper: https://ieeexplore.ieee.org/document/8716937
 class SquareLayer(nn.Module):
     def __init__(self):
         super().__init__()
@@ -16,10 +15,7 @@ class SquareLayer(nn.Module):
 
 class SCCNet(nn.Module):
     def __init__(self, numClasses=4, timeSample=438, Nu=22, C=22, Nc=20, Nt=1, dropoutRate=0.5,padding1=(0,0),padding2=(0,5)):
-        '''
-        The architecture of SCCNet consists of four blocks: 
-        the first convolution block, the second convolution block, the pooling block, and the softmax block.
-        '''
+
         super(SCCNet, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=Nu, kernel_size=(C, Nt), padding=padding1)
         self.batch_norm1 = nn.BatchNorm2d(Nu)
@@ -35,25 +31,18 @@ class SCCNet(nn.Module):
 
 
     def forward(self, x):
-        # logging.info('input shape: %s', x.shape)
         x = self.conv1(x)
-        # logging.info('conv1 shape: %s', x.shape)
         x = self.batch_norm1(x)
-        # logging.info('square shape: %s', x.shape)
         x = x.permute(0, 2, 1, 3)
-        # logging.info('permute shape: %s', x.shape)
+
         x =self.conv2(x)
-        # logging.info('conv2 shape: %s', x.shape)
         x = self.batch_norm2(x)
         x = self.square(x)
 
         x = self.dropout(x)
         x = self.pool(x)
-        # logging.info('pool shape: %s', x.shape)
         x = x.view(x.size(0), -1)
-        # logging.info('view shape: %s', x.shape)
         x = self.fc(x)
-        # logging.info('fc shape: %s', x.shape)
         return F.softmax(x, dim=1)
 
     # if needed, implement the get_size method for the in channel of fc layer
